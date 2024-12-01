73b52d1d80ffd39a89b6c4115ffb783a2cb72a93
EdgarAllenExile <riley.mercer4@gmail.com>
2024-12-01 16:58:32 +0800

    minor cosmetic updates

--- wezterm.lua
+++ wezterm.lua
@@ -1,5 +1,17 @@
 local wezterm = require("wezterm")
+local westerm_action = wezterm.action
+local wezterm_mux = wezterm.mux
+
 local config = wezterm.config_builder()
-config.font = wezterm.font("RobotoMono Nerd Font")
+
+-- UI
 config.color_scheme = "Sonokai (Gogh)"
+
+config.font = wezterm.font("RobotoMono Nerd Font")
+config.font_size = 13
+
+-- UI.TabBar
+config.use_fancy_tab_bar = true
+config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
+config.hide_tab_bar_if_only_one_tab = false
 return config
