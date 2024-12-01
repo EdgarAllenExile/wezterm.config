d454e907a9ced0db2697219d933fb27543cb3867
EdgarAllenExile <riley.mercer4@gmail.com>
2024-12-01 19:23:56 +0800

    add keybinds

--- wezterm.lua
+++ wezterm.lua
@@ -6,7 +6,6 @@ local config = wezterm.config_builder()
 
 -- UI
 config.color_scheme = "Sonokai (Gogh)"
-
 config.font = wezterm.font("RobotoMono Nerd Font")
 config.font_size = 13
 
@@ -14,4 +13,13 @@ config.font_size = 13
 config.use_fancy_tab_bar = true
 config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
 config.hide_tab_bar_if_only_one_tab = false
+
+-- Keybind
+config.keys = {
+	{
+		key = "r",
+		mods = "CMD",
+		action = wezterm.action.DisableDefaultAssignment,
+	},
+}
 return config
